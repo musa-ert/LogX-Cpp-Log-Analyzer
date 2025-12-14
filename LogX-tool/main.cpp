28442bbc66c30ae476f1e123138882dc2f410e6d
Musa Ertas <musaertas1420@gmail.com>
2025-12-14 23:04:28 +0300

    Log verilerini hafizada tutmak icin vektor eklendi

--- LogX-tool/main.cpp
+++ LogX-tool/main.cpp
@@ -2,10 +2,12 @@
 #include <fstream>
 #include <string>
 #include "logentry.h"
+#include <vector>
 using namespace std;
 
 int main()
 {
+    vector<logentry> hatasayisi;
     string o_dosya;
     cout<<"Welcome to Log Analysis Tool"<<endl;
     cout<<"Please enter a file name(look like this blabla.txt)"<<endl;
@@ -17,14 +19,16 @@ int main()
     }
     string satir;
     while(getline(dosyaoku,satir)){
+        if(satir.empty()) continue;
         logentry deneme(satir);
         if(deneme.getKontrol()=="[ERROR]"){
             deneme.printlog();
+            hatasayisi.push_back(deneme);
         }
 
     }
-
-
+    cout<<"-----------------------------------------";
+    cout<<"\n\t\tERROR counter:"<<hatasayisi.size()<<endl;
     dosyaoku.close();
     return 0;
 }
